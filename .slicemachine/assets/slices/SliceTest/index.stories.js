88b7fce582255b2bbcebb3b28ba8e4022dce9e22
ivanvinicius <ivanvinicius@github.com>
2022-03-25 10:36:01 -0300

    feat: PRISMIC slice machine

---
+++ .slicemachine/assets/slices/SliceTest/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/SliceTest';
+
+export default {
+  title: 'slices/SliceTest'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"slice_test","items":[],"primary":{"title":[{"type":"heading1","text":"Incubate killer web services","spans":[]}],"description":[{"type":"paragraph","text":"Sunt minim quis eiusmod ea esse. Consectetur proident aliqua duis dolor incididunt sint proident consectetur velit ea est eu.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
